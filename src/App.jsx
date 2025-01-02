a9d984268a708f4b679c15da5bf28327801d9b7c
sujalshrestha10 <suzolshrestha@gmail.com>
2025-01-02 19:22:07 +0545

    'react'

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import "./index.css";
+import Dokan from "./components/Dokan.jsx";
+
+function App() {
+  let userName = {
+    name: "aalu jasto k ",
+    work: "mafiaa",
+  };
+  console.log(userName);
+  return (
+    <>
+      <Dokan name="k xa k xa k xa k xa" />
+    </>
+  );
+}
+
+export default App;
